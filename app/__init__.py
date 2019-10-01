f6272519ae7900839d5fb61382c0ca260de0ba43
Kelechi David Igbokwe <Kelechi.igbokwe@actgruppe.de>
2019-10-01 11:16:50 +0200

    set access control dor the API

--- app/__init__.py
+++ app/__init__.py
@@ -14,11 +14,18 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+
+    @app.after_request
+    def inject_cors_header(response):
+       response.headers['Access-Control-Allow-Origin'] = '*'
+       response.headers['Access-Control-Allow-Methods'] = 'PUT, POST, PATCH, DELETE, GET'
+       response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'  # noqa
+       return response
     return app
 
 
 app = create_app(os.getenv("FLASK_CONFIG") or "default")
-CORS(app, resources={r"/api*": {"origins": "*"}})
+CORS(app, resources={r"/api*": {"origins": "*"}}, headers="Content-Type")
 app.jinja_env.globals.update(sub_path=sub_path)
 
 from . import views
